b37dc5e2f9235eecfb7cd0f4d51529bfd37f493a
lol <mdr>
2018-02-08 18:07:45 +0100

    test

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import chess
 import IA.MinMax as Mm
-
+import os
 board = chess.Board()
 
 count = 0
@@ -8,9 +8,10 @@ while not board.is_checkmate():
     # (move, dontknow) = engine.go()
     # board.push_uci(move.__str__())
     # engine.position(board.fen())
-    board.push_uci(Mm.best_play(board, 2).uci())
+    board.push_uci(Mm.best_play(board, 1).uci())
     count += 1
     # engine.position(board.fen())
     print(board)
+    os.system("clear")
     print("---------------------------------")
     print(count)
